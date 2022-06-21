eabbc4d1925607dbaba053ecdb473a5d71e40d46
tiwarisun <suniltiwari460@gmail.com>
2022-06-21 22:15:02 +0530

    blogproject

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -13,4 +13,26 @@ let createBlog = async function (req, res) {
 }
 
 
+
+const deleteblog= async function(req, res) {    
+try {  
+    
+    let blogId = req.params.blogId
+    let blog = await blogModel.findById(blogId)
+    if(!blog) {
+        return res.status(404).send({status: false, message: "no such blog exists"})
+    } else{  
+    let updatedblog = await blogModel.findOneAndUpdate({_id: blogId}, {$set:{isDeleted: true}}, {new: true})
+    res.status(200).send({status: true })
+  } 
+} catch (err) {
+ 
+    
+return res.status(500).send({status: false, message: "server err"})
+
+}
+}
+
 module.exports.createBlog = createBlog;
+module.exports.deleteblog = deleteblog
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -10,5 +10,7 @@ router.get("/test-me", function (req, res) {
 
 router.post("/authors", authorController.createAuthor)
 router.post("/blogs", blogController.createBlog)
+//router.put("/blogs/:blogId", blogController.updatedblog)
+router.delete("/blogs/:blogId",blogController.deleteblog)
 
 module.exports = router;
